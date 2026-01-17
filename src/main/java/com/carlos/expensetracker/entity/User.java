e66564273102e459d498e4d20be2e46f6cfb663d
Carlos <paulonluis222@gmail.com>
2026-01-16 23:08:05 -0300

    feat(entity): create User JPA entity with UUID and role enum

---
+++ src/main/java/com/carlos/expensetracker/entity/User.java
@@ -0,0 +1,41 @@
+package com.carlos.expensetracker.entity;
+
+import com.carlos.expensetracker.entity.enums.UserRole;
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role = UserRole.USER; //enum
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+
+}
