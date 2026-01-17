b1c0465d5c5d777abaedb1093db6cf3b5e7965ee
Carlos <paulonluis222@gmail.com>
2026-01-16 23:07:24 -0300

    feat(domain): add UserRole and ExpenseCategory enums

---
+++ src/main/java/com/carlos/expensetracker/entity/enums/ExpenseCategory.java
@@ -0,0 +1,14 @@
+package com.carlos.expensetracker.entity.enums;
+
+//TODO: revisar enum
+public enum ExpenseCategory {
+    GROCERIES,
+    LEISURE,
+    ELECTRONICS,
+    UTILITIES,
+    CLOTHING,
+    HEALTH,
+    TRANSPORT,
+    ENTERTAINMENT,
+    OTHERS
+}
---
+++ src/main/java/com/carlos/expensetracker/entity/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.carlos.expensetracker.entity.enums;
+
+public enum UserRole {
+    USER, ADMIN
+}
